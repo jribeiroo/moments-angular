7170a499b91339ada7e8c1ca1c24439dbe3343cd
jribeiroo <nicolas.jnr10@outlook.com>
2022-12-05 00:43:35 -0300

    feat(edit-moment):  Cria tela de edição
    Criando tela de edição
    
    Fixes #38

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MessagesComponent } from './components/messages/messages.component';
 import { MomentComponent } from './components/pages/moment/moment.component';
+import { EditMomentComponent } from './components/pages/edit-moment/edit-moment.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { MomentComponent } from './components/pages/moment/moment.component';
     NewMomentComponent,
     MomentFormComponent,
     MessagesComponent,
-    MomentComponent
+    MomentComponent,
+    EditMomentComponent
   ],
   imports: [
     HttpClientModule,
